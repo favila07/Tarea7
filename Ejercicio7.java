0098d25d6046dedce5cde306fd0f4c7a9d811774
favila07 <fjavila@uth.hn>
2024-11-03 17:42:06 -0600

    Add files via upload
    Imprima mediante un ciclo los números pares del 2 al 100

---
+++ Ejercicio7.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio7;
+
+/**
+ *
+ * @author Real Madrid
+ */
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 2; i <= 100; i+= 2){
+            
+            System.out.println(i);
+        }
+    }
+}
